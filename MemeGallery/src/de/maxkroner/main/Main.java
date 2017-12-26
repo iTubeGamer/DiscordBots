2485cafd64bf6c4dbdfad97348f249a6fb5b6a53
Max Kroner <krone@DESKTOP-UHDETPA.fritz.box>
2017-12-26 01:45:36 +0100

    added Error message

--- MemeGallery/src/de/maxkroner/main/Main.java
+++ MemeGallery/src/de/maxkroner/main/Main.java
@@ -15,40 +15,53 @@ public class Main {
 	private static Bot bot;
 
 	public static void main(String[] args) {
-		switch (args.length){
-		case 0: System.out.println("Please provide a token as argument.");
-				System.exit(1);
-				break;
-		case 1: startBotLaunchMenue(args[0]);
-		case 2: startBot(args[0], args[1]);
+		switch (args.length) {
+		case 0:
+			System.out.println("Please provide a token as argument.");
+			System.exit(1);
+			break;
+		case 1:
+			startBotLaunchMenue(args[0]);
+		case 2:
+			startBot(args[0], args[1]);
 		}
-		
+
 	}
 
 	private static void startBot(String botName, String token) {
-		switch(botName){
-		case "JokeBot": new JokeBot(token, scanner, userInput); break;
-		case "PrivateBot": new PrivateBot(token, scanner, userInput); break;
-		case "ScamBot": new ScamBot(token, scanner, userInput); break;
+		switch (botName) {
+		case "JokeBot":
+			new JokeBot(token, scanner, userInput);
+			break;
+		case "PrivateBot":
+			new PrivateBot(token, scanner, userInput);
+			break;
+		case "ScamBot":
+			new ScamBot(token, scanner, userInput);
+			break;
 		}
-		
+
 	}
 
 	private static void startBotLaunchMenue(String token) {
 
-		int auswahl = userInput.getMultipleChoiceResult("Which bot should be started?", "JokeBot", "PrivateBot", "ScamBot");
+		int auswahl = userInput.getMultipleChoiceResult("Which bot should be started?", "JokeBot",
+				"PrivateBot", "ScamBot");
 		switch (auswahl) {
-			case 1:
-				new JokeBot(token, scanner, userInput);
-				break;
-			case 2:
-				new PrivateBot(token, scanner, userInput);
-				break;
-			case 3:
-				new ScamBot(token, scanner, userInput);
-				break;
+		case 1:
+			new JokeBot(token, scanner, userInput);
+			break;
+		case 2:
+			new PrivateBot(token, scanner, userInput);
+			break;
+		case 3:
+			new ScamBot(token, scanner, userInput);
+			break;
 		}
 
+		System.out.println(
+				"The argument containted no valid bot name. Please provide either one argument with the token or a bot name in the first argument and the token in the second argument.");
+		System.exit(1);
 	}
 
 	public static void exit() {
